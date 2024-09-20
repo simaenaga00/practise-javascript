534a737416272bb03fc60dd91a260855e196931a
simaenaga00 <makomakochin0328@gmail.com>
2024-09-20 04:51:11 +0000

    9月19日分を別ファイルに移動

---
+++ 9_19/main.js
@@ -0,0 +1,15 @@
+document.querySelector("h1").textContent = "南村真"
+document.querySelector("h2").textContent = "南村真"
+/*documentとは！*/
+/*htmlファイル内で書かれているコードを一つのオブジェクトとしてみなす。*/
+
+//DOM(ドム)
+/*ドキュメントオブジェクトモデルの略で、webページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+//上記コードの意味
+//document.querySelector("h1").textContent = "南村真"
+
+//ドキュメント内の("h1")タグを探してね
+//document.querySelector("h1")
+
+//h1テキストの中身のtextを変えてね
+//textContent = "南村真"
