58fdcf335b0af650e8bd84dc6c1ecdc6f7be25a6
simaenaga00 <makomakochin0328@gmail.com>
2024-10-18 06:18:25 +0000

    10_18 ゲームを作った

--- main.js
+++ main.js
@@ -1,31 +1,75 @@
-//クリックされたら動作する
+const gameSelector = document.getElementById('game-selector');
+const gameContainer = document.getElementById('game-container');
 
-function errorInput(){
-    const month = document.getElementById("month").value
-    const day = document.getElementById("day").value
-    if(month<1 || month>12){
-        alert("適切な値ではありません")
-        return
+gameContainer.textContent="ゲームを選ぶとここに表示されます"
+
+gameSelector.addEventListener("change",function(){
+    gameContainer.innerHTML = "";
+    switch(gameSelector.value){
+        case "none":
+            none()   
+            break;
+        case "click-counter":
+            ClickCounterGame();    
+            break;
+        case "number-guess":
+            NumberGuess();
+            break;
     }
-    console.log("Month:",month,"Day:",day);
-    
+})
+function none(){
+gameContainer.textContent="ゲームを選ぶとここに表示されます"
 }
 
-const monthSelect = document.getElementById("month2");
-monthSelectList();
-monthSelectList(){
-    for(let i=1;i<12;i++){
-        let option = document.createElement("option")
-        option.value = i;
-        option.textContent = i;
-        monthSelect,appendChild(option);
-    }
-}
-//document.getElementById("month")はタグを確認するだけ
-//valueはそのタグの値を示す
+function NumberGuessGame(){
+    const randomNumber = Math.floor(Math.random()*100)+1;
+    let message = document.createElement("p");
+    let imput = document.createElement("input");
+    input.type = "namber";
+    input.max = 100;
+    input.tmin= 1;
+    input.type = "namber";
+    let button = document.createElement("button");
+    massage.textContent = "文字"
 
-/*
-javaScriptのプログラムとHTMLファイルはどこで対応しているか
-タグのついているIDで紐づく
-タグの中にある付加情報のことを属性という
-*/
+    gameContainer.appendChild(input);
+    gameContainer.appendChild(button);
+    gameContainer.appendChild(massage);
+}
+
+function ClickCounterGame(){
+
+    let count = 0;
+
+    let button1 = document.createElement("button");
+    button1.textContent = "+1ボタン";
+
+    let button2 = document.createElement("button");
+    button2.textContent = "+10ボタン";
+
+    let button3 = document.createElement("button");
+    button3.textContent = "+100ボタン";
+
+    let counter = document.createElement("p");
+    counter.textContent = count;
+
+        button1.addEventListener("click",function(){
+        count++;
+        counter.textContent = count;
+    })
+
+        button2.addEventListener("click",function(){
+        count=count+10;
+        counter.textContent = count;
+    })
+    button3.addEventListener("click",function(){
+        count=count+100;
+        counter.textContent = count;
+    })
+
+    gameContainer.appendChild(button1);
+    gameContainer.appendChild(button2);
+    gameContainer.appendChild(counter);
+}
+
+
