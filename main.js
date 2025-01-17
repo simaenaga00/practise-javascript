2f87b8f5c504889cae45021ff6a0ff46aef86583
simaenaga00 <makomakochin0328@gmail.com>
2025-01-17 06:17:52 +0000

    數あてゲームの追加

--- main.js
+++ main.js
@@ -12,11 +12,15 @@ function ClickCounterGame(){
     let counter = document.createElement("p");
     counter.textContent = count;
         button1.addEventListener("click",function(){
-        count++;
+        if(count < 100){
+            count++;
+        }
         counter.textContent = count;
     })
         button2.addEventListener("click",function(){
-        count=count+10;
+        if(count <= 90){
+            count=count+10;
+        }
         counter.textContent = count;
     })
         button3.addEventListener("click",function(){
@@ -34,3 +38,18 @@ function ClickCounterGame(){
     gameContainer.appendChild(counter);
 }
 ClickCounterGame();
+
+function startNumberGuessGame(){
+    const gameContainer = document.getElementById("game-container");
+    const randomNumber = Math.floor(Math.random()*100)+1;
+    let message = document.createElement("p");
+    message.textContent = randomNumber
+    let input = document.createElement("input");
+    input.type = "namber";
+    input.max = 100;
+    input.tmin= 1;
+    input.placeholder = '好きな数字を入力してください(1~100)';
+    gameContainer.appendChild(message);
+    gameContainer.appendChild(input);
+}
+startNumberGuessGame();
